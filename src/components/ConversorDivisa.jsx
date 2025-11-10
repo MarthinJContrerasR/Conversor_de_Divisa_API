7f420d93180f54921f0856b9d645a580da73e636
Marthín José Contreras Rodríguez <Marthin J Contreras>
2025-11-09 23:13:00 -0600

    Funcion useEffect para hacer el llamado de la API.

--- src/components/ConversorDivisa.jsx
+++ src/components/ConversorDivisa.jsx
@@ -1,12 +1,33 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect} from "react";
 import "./ConversorDivisa.css";
 
 export function ConversorDivisa() {
     
+    //VARIABLES DEL PROGRAMA
+    const [valorCambio, setValorCambio]=useState(null); //Nueva Variable para almacenar la taza de Cambio de la API
     const dolarRef = useRef();
     const [resultado, setResultado] = useState("");
     const [dolares, setDolares] = useState("");
 
+
+    useEffect( ()=>{
+        const llamadoAPICambio=async()=>{
+            try {
+                const respuesta= await fetch("https://api.frankfurter.dev/v1/2024-01-01..?symbols=USD");
+                const datos = await respuesta.json();
+
+                console.log(datos);
+
+
+                setValorCambio(datos.rates.USD)
+            } catch (error) {
+                console.error("Error al acceder a la API: ", error);
+            }
+        };
+        llamadoAPICambio();
+    },[]);
+
+    //CONVERSION DE LA DIVISA
     const conversorMoneda = () => {
         const valorDolar = parseFloat(dolarRef.current.value);
         
@@ -16,11 +37,11 @@ export function ConversorDivisa() {
             return;
         }
 
-        const valorResultado = (valorDolar * 26.15).toFixed(2);
+        const valorResultado = (valorDolar * valorCambio).toFixed(2);
         setDolares(valorDolar.toString());
         setResultado(`${valorResultado} Lempiras`);
     }
-
+    //Boton de Limpiar el campo 
     const limpiarCampos = () => {
         setResultado("");
         setDolares("");
@@ -29,7 +50,7 @@ export function ConversorDivisa() {
     
     return (
         <div className="conversor">
-            <h2>TASA DE CAMBIO DÓLAR A LEMPIRA: <br /> 26.15</h2>
+            <h2>TASA DE CAMBIO DÓLAR A LEMPIRA: <br /> {valorCambio} </h2>
             <h2>Conversor de Dólares a Lempiras</h2>
             <input type="text" placeholder="Ingrese la Cantidad de Dólares a Convertir" ref={dolarRef} /><br/><br/>
             <button onClick={conversorMoneda}>Convertir</button> 
